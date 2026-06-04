6267f0a9ce6081d18b7c06c1deb92116359ccf8a
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-06-04 18:31:11 -0300

    Disable Astra client only by default

--- data/server_config.lua
+++ data/server_config.lua
@@ -8,7 +8,7 @@ maxProtocolOutfits = 255
 maxAddonAttributes = 3
 dllCheckKick = true
 dllCheckKickTime = 5
-astraClientOnly = true
+astraClientOnly = false
 
 -- Dual Wielding
 -- NOTE: dualWieldingSpeedRate = 200 means dual-wielding attacks twice as fast
